7dc844a74a67edda40ff76189f7383a0a0e87297
martasolidadealmeida <martamarcio123@gmail.com>
2022-08-29 16:07:10 -0300

    atividades 1,2 e 3 concluidos

---
+++ exercicio2/script.js
@@ -0,0 +1,13 @@
+let perguntaComida = "voçe gosta de sopa?"
+let respostaComida = prompt(perguntaComida)
+
+let perguntaTemperatura = "voe esta com frio?"
+let respostaTemperatura = prompt(perguntaTemperatura)
+
+let perguntaBanho = "voçe ja tomou banho hoje?"
+let respostaBanho = prompt(perguntaBanho)
+
+console.log(perguntaComida, respostaComida)
+console.log(perguntaTemperatura, respostaTemperatura)
+console.log(perguntaBanho, perguntaBanho)
+
---
+++ exercicio3/script.js
@@ -0,0 +1,8 @@
+let a = 20
+let b = 15
+
+let A = b
+let B = a
+
+console.log("o novo valor de da a é", B)
+console.log("o novo valor de b é", A)
